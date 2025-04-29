af486264506190dff4df8f4547ae9f52de28ec0e
henrique <henrique.rebolho@gmail.com>
2025-04-29 16:02:19 +0200

    Initial commit

---
+++ src/main/java/aiss/GithubMiner/GithubMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.GithubMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubMinerApplication.class, args);
+	}
+
+}
