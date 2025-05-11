55e0fb08b3462425e649036aa7c44e1a05e1c0d2
Ángela Valderas <angvalfer1@alum.us.es>
2025-05-11 14:19:59 +0200

    Controllers

--- src/main/java/aiss/GithubMiner/Controllers/CommitController.java
+++ src/main/java/aiss/GithubMiner/Controllers/CommitController.java
@@ -1,4 +1,44 @@
 package aiss.GithubMiner.Controllers;
 
+import aiss.GithubMiner.model.gitminer.MinerCommit;
+import aiss.GithubMiner.service.CommitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/github")
 public class CommitController {
+
+    @Autowired
+    CommitService commitService;
+
+
+
+    @GetMapping("/{workspace}/{repo_slug}")
+    public List<MinerCommit> getCommitsFromFixedRoute(
+            @PathVariable String workspace,
+            @PathVariable("repo_slug") String repoSlug,
+            @RequestParam(defaultValue = "5") int nCommits,
+            @RequestParam(defaultValue = "2") int maxPages
+    ) {
+        return commitService.getCommits(workspace, repoSlug, nCommits, maxPages);
+    }
+
+    @GetMapping("/commits/{id}")
+    public MinerCommit getCommit(@PathVariable String id) {
+        return commitService.getCommitById("gentlero", "github-api", id);
+    }
+
+    @PostMapping("/{workspace}/{repo_slug}")
+    public String sendCommitsToGitMiner(
+            @PathVariable String workspace,
+            @PathVariable("repo_slug") String repoSlug,
+            @RequestParam(defaultValue = "5") int nCommits,
+            @RequestParam(defaultValue = "2") int maxPages
+    ) {
+        int saved = commitService.sendCommitsToGitMiner(workspace, repoSlug, nCommits, maxPages);
+        return saved + " commits enviados a GitMiner correctamente.";
+    }
 }
--- src/main/java/aiss/GithubMiner/Controllers/IssueController.java
+++ src/main/java/aiss/GithubMiner/Controllers/IssueController.java
@@ -1,4 +1,42 @@
 package aiss.GithubMiner.Controllers;
 
+import aiss.GithubMiner.model.gitminer.MinerIssue;
+import aiss.GithubMiner.service.IssueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/github")
 public class IssueController {
+
+    @Autowired
+    IssueService issueService;
+
+    @GetMapping("/{workspace}/{repo_slug}/issues")
+    public List<MinerIssue> getIssues(
+            @PathVariable String workspace,
+            @PathVariable("repo_slug") String repoSlug,
+            @RequestParam(defaultValue = "5") int nIssues,
+            @RequestParam(defaultValue = "2") int maxPages
+    ) {
+        return issueService.getIssues(workspace, repoSlug, nIssues, maxPages);
+    }
+
+    @GetMapping("/issues/{id}")
+    public MinerIssue getIssue(@PathVariable String id) {
+        return issueService.getIssueById("gentlero", "github-api", id);
+    }
+
+    @PostMapping("/{workspace}/{repo_slug}/issues")
+    public String sendIssuesToGitMiner(
+            @PathVariable String workspace,
+            @PathVariable("repo_slug") String repoSlug,
+            @RequestParam(defaultValue = "5") int nIssues,
+            @RequestParam(defaultValue = "2") int maxPages
+    ) {
+        int saved = issueService.sendIssuesToGitMiner(workspace, repoSlug, nIssues, maxPages);
+        return saved + " issues enviados a GitMiner correctamente.";
+    }
 }
